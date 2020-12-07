5c0ded8d9b53e202d4b34d7aaedc3a1f8e48eddb
japsasir <japsasir@gmail.com>
2020-12-07 12:32:46 +0100

    Grupos y textos integrados

--- directorios.sh
+++ directorios.sh
@@ -6,7 +6,7 @@
 # Configuramos el script para que muestre los comandos por consola
 set -x
 
-## Creamos los usuarios.
+###-----Creamos los usuarios.-----###
 
 # Creamos el usuario ATC1.
 useradd -p practicas ATC1
@@ -21,12 +21,37 @@ useradd -p practicas PTC1
 
 # Creamos el grupo ATC. Recordemos que PTC no tendrá grupo.
 groupadd ATC
+groupadd ALL
 
 # Añadimos los usuarios ATC1, ATC2, ATC3 al grupo ATC.
 usermod -a -G ATC ATC1
 usermod -a -G ATC ATC2
 usermod -a -G ATC ATC3
 
-## Opcional: Comprobar que se ha aplicado correctamente con
-# groups ATC1
+# Añadimos todos los usuarios al grupo ALL
+usermod -a -G ALL ATC1
+usermod -a -G ALL ATC2
+usermod -a -G ALL ATC3
+usermod -a -G ALL PTC1
 
+## Opcional: Comprobar que se ha aplicado correctamente con 'groups ATC1'
+
+###-----Creamos los directorios. Introducimos un archivo de texto personalizado donde corresponde.-----###
+mkdir /home/asignaturas
+mkdir /home/asignaturas/tecnologia
+mkdir /home/asignaturas/tecnologia/profesor
+mkdir /home/asignaturas/tecnologia/profesor/notas
+echo "Estas son las notas de los alumnos." > /home/asignaturas/tecnologia/profesor/notas/Notas.txt
+mkdir /home/asignaturas/tecnologia/profesor/examenes
+echo "Estos son los examenes de los alumnos." > /home/asignaturas/tecnologia/profesor/examenes/Examen.txt
+mkdir /home/asignaturas/tecnologia/alumnos
+mkdir /home/asignaturas/tecnologia/alumnos/apuntes
+echo "Estos son los apuntes de los alumnos." > /home/asignaturas/tecnologia/alumnos/apuntes/Apuntes.txt
+mkdir /home/asignaturas/tecnologia/alumnos/sugerencias
+echo "Estas son las sugerencias de los alumnos." > /home/asignaturas/tecnologia/alumnos/sugerencias/Sugerencias.txt
+
+###-----Asignamos permisos a los directorios.-----###
+
+
+
+###-----Comprobación final.-----###
